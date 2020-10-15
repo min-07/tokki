0a80768e8520c718fdd9bc57b7b6db2c6d92f232
tokki <mjluckyplay@gmail.com>
2020-10-15 16:38:15 +0900

    add message.content

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on('message', (message) => {
     return message.reply('펀,쿨,섹시한 우리만의 수장님이자 디코최고 존잘남!그리고 옷 좋아함ㅋㅋㄹ');
   }
 
-  if(message.content == '!도와줘') {
+  if(message.content == '!김토끼 정보') {
     let img = 'https://media.discordapp.net/attachments/745482814987698290/762501940143325204/20201005_113013.jpg';
     let embed = new Discord.RichEmbed()
       .setTitle('타이틀')
