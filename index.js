054669d29146d74a0b4d392284c65825ec1eb4ff
tokki <mjluckyplay@gmail.com>
2020-10-18 16:27:18 +0900

    add message.content

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ client.on('message', (message) => {
   if(message.content == '사랑해') {
     return message.reply('ㅈㄹ');
   }
-
+  if(message.content == '!김토끼 얼굴') {
+    return message.reply('https://media.discordapp.net/attachments/761390715670364190/767287432843886622/8.PNG');
+  }
   if(message.content == '!김토끼 정보') {
     let img = 'https://media.discordapp.net/attachments/745482814987698290/762501940143325204/20201005_113013.jpg';
     let embed = new Discord.RichEmbed()
